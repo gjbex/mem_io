eec25674cef614439470119ea552454933f3446a
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-04-06 20:26:30 +0200

    Added log removal to clean scripts

--- examples/bash/clean.sh
+++ examples/bash/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -f redis_*.conf redis_*.rdb
+rm -f redis_*.conf redis_*.rdb redis_*.log
 rm -f mem_io_*.conf
 rm -f data.txt
--- examples/worker/clean.sh
+++ examples/worker/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f redis_*.conf redis_*.rdb
+rm -f redis_*.conf redis_*.rdb redis_*.log
 rm -f mem_io_*.conf
 rm -f data.txt
 rm -f job.pbs.*
